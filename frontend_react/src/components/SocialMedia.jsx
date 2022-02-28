2459a3750bba66b725441bc6c452a8bf2852ed32
issdq <work.issdq@gmail.com>
2022-03-01 06:04:41 +0700

    final update

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaGithub, FaTwitter } from 'react-icons/fa';
+import { BsTwitter, BsInstagram, BsGithub } from 'react-icons/bs';
+// import { FaGithub } from 'react-icons/fa';
 
 const SocialMedia = () => (
 	<div className="app__social">
 		<div>
 			<a href="https://twitter.com/stwgrffn">
-				<FaTwitter />
+				<BsTwitter />
 			</a>
 		</div>
 		<div>
 			<a href="https://github.com/eulbyvan">
-				<FaGithub />
+				<BsGithub />
 			</a>
 		</div>
 		<div>
