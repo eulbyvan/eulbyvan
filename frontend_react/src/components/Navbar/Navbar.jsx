d3f3c00c885c1d1f84fbc1d8cc37649bb914d6f0
issdq <work.issdq@gmail.com>
2022-03-02 04:56:44 +0700

    update navbar when scrolling

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { HiMenuAlt4, HiX } from 'react-icons/hi';
 import { motion } from 'framer-motion';
 
@@ -6,45 +6,73 @@ import { images } from '../../constants';
 import './Navbar.scss';
 
 const Navbar = () => {
-  const [toggle, setToggle] = useState(false);
+	const [toggle, setToggle] = useState(false);
+	const [prevScrollPos, setPrevScrollPos] = useState(0);
+	const [visible, setVisible] = useState(true);
 
-  return (
-    <nav className="app__navbar">
-      <div className="app__navbar-logo">
-        <img src={images.logo} alt="logo" />
-      </div>
-      <ul className="app__navbar-links">
-        {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
-          <li className="app__flex p-text" key={`link-${item}`}>
-            <div />
-            <a href={`#${item}`}>{item}</a>
-          </li>
-        ))}
-      </ul>
+	const handleScroll = useCallback(() => {
+		const currentScrollPos = window.pageYOffset;
 
-      <div className="app__navbar-menu">
-        <HiMenuAlt4 onClick={() => setToggle(true)} />
+		setVisible(
+			(prevScrollPos > currentScrollPos &&
+				prevScrollPos - currentScrollPos > 70) ||
+				currentScrollPos < 10
+		);
 
-        {toggle && (
-          <motion.div
-            whileInView={{ x: [300, 0] }}
-            transition={{ duration: 0.85, ease: 'easeOut' }}
-          >
-            <HiX onClick={() => setToggle(false)} />
-            <ul>
-              {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
-                <li key={item}>
-                  <a href={`#${item}`} onClick={() => setToggle(false)}>
-                    {item}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </motion.div>
-        )}
-      </div>
-    </nav>
-  );
+		setPrevScrollPos(currentScrollPos);
+	}, [prevScrollPos]);
+
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll);
+
+		return () => window.removeEventListener('scroll', handleScroll);
+	}, [prevScrollPos, visible, handleScroll]);
+
+	return (
+		<nav
+			className="app__navbar"
+			style={{ transition: 'top 0.6s', top: visible ? '0' : '-60px' }}
+		>
+			<div className="app__navbar-logo">
+				<img src={images.logo} alt="logo" />
+			</div>
+			<ul className="app__navbar-links">
+				{['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+					<li className="app__flex p-text" key={`link-${item}`}>
+						<div />
+						<a href={`#${item}`}>{item}</a>
+					</li>
+				))}
+			</ul>
+
+			<div className="app__navbar-menu">
+				<HiMenuAlt4 onClick={() => setToggle(true)} />
+
+				{toggle && (
+					<motion.div
+						whileInView={{ x: [300, 0] }}
+						transition={{ duration: 0.85, ease: 'easeOut' }}
+					>
+						<HiX onClick={() => setToggle(false)} />
+						<ul>
+							{['home', 'about', 'work', 'skills', 'contact'].map(
+								(item) => (
+									<li key={item}>
+										<a
+											href={`#${item}`}
+											onClick={() => setToggle(false)}
+										>
+											{item}
+										</a>
+									</li>
+								)
+							)}
+						</ul>
+					</motion.div>
+				)}
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
