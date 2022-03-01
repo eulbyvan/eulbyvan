52c066124a2fe3662f7f2cfb54f51a7a3c33f2e4
issdq <work.issdq@gmail.com>
2022-03-01 16:52:41 +0700

    update wa.me link

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -53,7 +53,7 @@ const Footer = () => {
 				</div>
 				<div className="app__footer-card">
 					<img src={images.mobile} alt="phone" />
-					<a href="wa.me/+6281280618031" className="p-text">
+					<a href="https://wa.me/+6281280618031" className="p-text">
 						+62 812-8061-8031
 					</a>
 				</div>
