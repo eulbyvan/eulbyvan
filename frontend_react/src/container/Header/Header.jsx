8287b44ca94a07f0ce3b952e2a24b0fc9161efb2
issdq <work.issdq@gmail.com>
2022-04-04 16:22:33 +0700

    Update Header.jsx

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -59,11 +59,16 @@ const Header = () => (
 			whileInView={scaleVariants.whileInView}
 			className="app__header-circles"
 		>
-			{[images.git, images.python, images.node].map((circle, index) => (
-				<div className="circle-cmp app__flex" key={`circle-${index}`}>
-					<img src={circle} alt="profile_bg" />
-				</div>
-			))}
+			{[images.git, images.csharp, images.sqlserver].map(
+				(circle, index) => (
+					<div
+						className="circle-cmp app__flex"
+						key={`circle-${index}`}
+					>
+						<img src={circle} alt="profile_bg" />
+					</div>
+				)
+			)}
 		</motion.div>
 	</div>
 );
