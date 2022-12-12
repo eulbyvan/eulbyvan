ee456b663e548a309f92d6d3b63d95f3affd2479
eulbyvan <work.issdq@gmail.com>
2022-12-13 03:55:38 +0700

    Update Header.jsx

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -59,7 +59,7 @@ const Header = () => (
 			whileInView={scaleVariants.whileInView}
 			className="app__header-circles"
 		>
-			{[images.springboot, images.react, images.java].map((circle, index) => (
+			{[images.springboot, images.react, images.git].map((circle, index) => (
 				<div className="circle-cmp app__flex" key={`circle-${index}`}>
 					<img src={circle} alt="profile_bg" />
 				</div>
